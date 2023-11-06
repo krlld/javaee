52127493c630ba898298c81d6af401cd6b1344f8
krlld <kirilldikun25@gmail.com>
2023-11-06 18:58:01 +0300

    Add checking for blank strings

--- src/main/java/by/teachmeskills/lesson26/service/ValidateService.java
+++ src/main/java/by/teachmeskills/lesson26/service/ValidateService.java
@@ -3,10 +3,10 @@ package by.teachmeskills.lesson26.service;
 public class ValidateService {
 
     public static boolean isValidName(String name) {
-        return name != null && !name.isEmpty();
+        return name != null && !name.trim().isEmpty();
     }
 
     public static boolean isValidDescription(String description) {
-        return description != null && !description.isEmpty();
+        return description != null && !description.trim().isEmpty();
     }
 }
