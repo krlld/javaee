1c313ed595bdd550ade4bf83f040249364b2db90
krlld <kirilldikun25@gmail.com>
2024-01-05 21:54:20 +0300

    Add lesson 40

--- javaee/src/main/java/by/teachmeskills/lesson39/WebConfig.java
+++ javaee/src/main/java/by/teachmeskills/lesson39/WebConfig.java
@@ -1,11 +1,23 @@
 package by.teachmeskills.lesson39;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan
 public class WebConfig {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
+    }
 }
--- javaee/src/main/java/by/teachmeskills/lesson39/domain/Picture.java
+++ javaee/src/main/java/by/teachmeskills/lesson39/domain/Picture.java
@@ -1,5 +1,8 @@
 package by.teachmeskills.lesson39.domain;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,10 +16,19 @@ public class Picture {
 
     private Long id;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
     private String name;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
     private String description;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
     private String url;
 
     public Picture(Picture picture) {
